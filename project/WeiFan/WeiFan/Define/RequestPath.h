6d8ca3dc71813e5158031f48e4b11f9486754ea6
WQ944564251 <13419642408@163.com>
2016-07-20 09:03:53 +0800

    添加了RequestPath.h

---
+++ project/WeiFan/WeiFan/Define/RequestPath.h
@@ -0,0 +1,15 @@
+//
+//  RequestPath.h
+//  WeiFan
+//
+//  Created by tarena50 on 16/7/20.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#ifndef RequestPath_h
+#define RequestPath_h
+
+#define kBasePath @""
+
+
+#endif /* RequestPath_h */
